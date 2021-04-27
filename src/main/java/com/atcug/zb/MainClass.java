9993dac28cc5137b5da56fcf4b2ffc31085eb9ee
zb <1252324263@qq.com>
2021-04-27 17:44:03 +0800

    hot-fix commit2

--- src/main/java/com/atcug/zb/MainClass.java
+++ src/main/java/com/atcug/zb/MainClass.java
@@ -12,6 +12,7 @@ public class MainClass {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello git4");
+        System.out.println("hot-fix test");
     }
 
 }
