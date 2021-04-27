a1ffdf76fa75593da3550dfeafe1a5461a29c3a2
zb <1252324263@qq.com>
2021-04-27 17:35:06 +0800

    third commit

--- src/main/java/com/atcug/zb/MainClass.java
+++ src/main/java/com/atcug/zb/MainClass.java
@@ -10,6 +10,7 @@ public class MainClass {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git2");
+        System.out.println("hello git3");
     }
 
 }
