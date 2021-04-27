ce17a40d0b86e54e2327c6e92fff0318f056687d
zb <1252324263@qq.com>
2021-04-27 17:30:10 +0800

    first commit

---
+++ src/main/java/com/atcug/zb/MainClass.java
@@ -0,0 +1,14 @@
+package com.atcug.zb;
+
+/**
+ * @author Abin
+ * @version 1.0
+ * @date 2021/4/27 17:26
+ */
+public class MainClass {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+
+}
